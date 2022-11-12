0005991d38fcdd86a1e5af4bb541bf4d87b26d11
Petrushov Denis <bam411@yandex.ru>
2022-11-12 17:32:25 +0300

    exercise 8

---
+++ exercise_8/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Введите число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = 1;
+
+while(numB < numA)
+{
+    Console.WriteLine(numB + 1);
+    numB = numB + 2;
+}
