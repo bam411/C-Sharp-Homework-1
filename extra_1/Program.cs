c2e553ffdc18538995d49fcd72c21ab0213eaa4a
Petrushov Denis <bam411@yandex.ru>
2022-11-15 15:59:34 +0300

    extra 1

---
+++ extra_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
+
+string secret = "Well done, boy!";
+string pswd = "password";
+int a = 1;
+
+while (a <= 3)
+{
+    Console.WriteLine("Please enter password: ");
+    string message = Console.ReadLine();
+    if (message == pswd)
+    {
+        Console.WriteLine(secret);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Password is incorrect! Try again");
+    }
+    a++;
+}
